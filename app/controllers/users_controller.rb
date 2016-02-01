4c65992bbf4371b8e18d8dad2dbcc087b1dcc795
karenjho <hokarenj@gmail.com>
2016-02-01 15:43:40 -0500

    Create User edit and show (profile page) views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -13,9 +15,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to posts_path, notice: "Your user information has been updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to posts_path, notice: "This user has been deleted from Dossier."
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def load_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "posts#index"
 
   resources :posts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 end
